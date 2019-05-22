42a252ad8a21d570e30f6f7f74a37f3da3b1477f
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-22 15:28:36 +0300

    style

--- src/script.js
+++ src/script.js
@@ -4,28 +4,30 @@ function createCalendar(id, year, month) {
     const containerForTable = document.querySelector(`#${id}`);
     const table = document.createElement('table');
     const thead = createTableHead(days);
-    table.appendChild(thead);
     const dataMonth = getDataMonth(year, month);
     const tbody = createTableBody(dataMonth);
+
+    table.appendChild(thead);
     table.appendChild(tbody);
     containerForTable.appendChild(table);
 }
 
 function getDataMonth(year, month) {
     const currentMonth = month - 1;
+
     const startDay = new Date(year, currentMonth).getDay();
     const quantityDays = new Date(year, month, 0).getDate();
-    return {
-        startDay,
-        quantityDays
-    }
+
+    return {startDay, quantityDays};
 }
 
 function createTableHead(days) {
     const thead = document.createElement('thead');
     const tr = document.createElement('tr');
+
     days.forEach((day) => {
         const th = document.createElement('th');
+
         th.innerText = day;
         tr.appendChild(th);
     });
@@ -34,11 +36,13 @@ function createTableHead(days) {
 
 function createTableBody(dataMonth) {
     const tbody = document.createElement('tbody');
+    const position = dataMonth.startDay - 1;
     let tr = document.createElement('tr');
+
     for (let i = 1; i < dataMonth.startDay; i++) {
         tr.appendChild(document.createElement('td'));
     }
-    const position = dataMonth.startDay - 1;
+
     for (let i = 1; i <= dataMonth.quantityDays; i++) {
         const td = document.createElement('td');
         td.textContent = `${i}`;
@@ -48,9 +52,11 @@ function createTableBody(dataMonth) {
             tr = document.createElement('tr');
         }
     }
+
     for(let i = tr.childNodes.length +1; i <= 7; i++ ) {
         tr.appendChild(document.createElement('td'));
     }
+
     tbody.appendChild(tr);
     return tbody;
 }
